6a1444a1ffc23a84522535144dba67f758d3019d
Zhangjiafeng <2387632660@qq.com>
2019-10-29 14:20:44 +0800

    完成assign_server

---
+++ client/src/client.java
@@ -0,0 +1,121 @@
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.net.*;
+import java.util.Date;
+
+
+public class client {
+    private static final int PORT=9001;
+    private static final int ASSIGN_PORT=9004;
+    public static void createAndShowGUI() throws UnknownHostException {
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        JFrame frame=new JFrame("文件传输");
+        frame.setSize(400,300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel panel1=new JPanel();
+        JLabel label=new JLabel("发送的文件");
+        panel1.add(label);
+        JTextField text=new JTextField(20);
+        text.setBounds(10,40,120,25);
+        JPanel panel2=new JPanel();
+        JFileChooser filechooser=new JFileChooser("D:\\");
+        filechooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+        panel2.add(filechooser);
+        frame.add(panel2,BorderLayout.CENTER);
+        JPanel panel3=new JPanel();
+        JLabel labels=new JLabel("文件大小");
+        panel3.add(labels);
+        JTextField size=new JTextField(10);
+        panel3.add(size);
+        JLabel label2=new JLabel("IP地址");
+        panel3.add(label2);
+        JTextField ipText=new JTextField(20);
+        panel3.add(ipText);
+        JButton btn=new JButton("发送");
+        panel3.add(btn);
+        frame.add(panel3, BorderLayout.SOUTH);
+        InetAddress addr=InetAddress.getLocalHost();
+        ipText.setText(addr.getHostAddress());
+        frame.pack();
+        frame.setVisible(true);
+        filechooser.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                System.out.println(filechooser.getSelectedFile().length());
+                size.setText(filechooser.getSelectedFile().length()/1024+"KB");
+            }
+        });
+        btn.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                System.out.println(text.getText());
+                InputStream in = null;
+                Socket socket = null;
+                try {
+                    File file = new File(filechooser.getSelectedFile().getPath());
+                    System.out.println(file.length());
+                    size.setText(file.length()/1024+"KB");
+                    if(file.exists() && file.isFile()) {
+                        in = new FileInputStream(file);
+                        socket = new Socket();
+                        socket.connect(new InetSocketAddress(ipText.getText(), ASSIGN_PORT));
+                        OutputStream out = socket.getOutputStream();
+                        out.write(file.getName().getBytes());
+                        out.write("?".getBytes());
+                        out.write(ipText.getText().getBytes());
+                        out.write("?".getBytes());
+                        out.write(String.valueOf(PORT).getBytes());
+                        out.write("?".getBytes());
+                        Date date = new Date();
+                        out.write(String.valueOf(date.getTime()).getBytes());
+                        out.write("?".getBytes());
+                        byte[] data=new byte[8];
+                        in.read(data);
+                        out.write(data,0,8);
+                        out.write("?".getBytes());
+                        out.write(String.valueOf(file.length()).getBytes());
+                        System.out.println("请等待分配文件服务器...");
+//                        int fileLength=0;
+//                        byte[] data = new byte[1024];
+//                        int i = 0;
+//                        while((i = in.read(data)) != -1) {
+//                            fileLength+=data.length;
+//
+//                            out.write(data, 0, i);
+//                        }
+                    }else {
+                        System.out.println("文件不存在或者一个文件~~");
+                    }
+                } catch (Exception exp) {
+                   System.out.println(exp);
+                }finally {
+                    try {
+                        in.close();
+                    } catch (IOException exp) {
+                        exp.printStackTrace();
+                    }finally {
+                        // 强制将输入流置为空
+                        in = null;
+                    }
+                    try {
+                        socket.close();
+                    } catch (IOException exp) {
+                        exp.printStackTrace();
+                    }finally {
+                        // 强制释放socket
+                        socket = null;
+                    }
+
+                }
+
+            }
+        });
+    }
+
+    public static void main(String[] args) throws IOException {
+        createAndShowGUI();
+    }
+}
---
+++ server/src/server.java
@@ -0,0 +1,143 @@
+
+import javafx.beans.binding.BooleanExpression;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.net.*;
+import java.nio.file.Path;
+import java.util.InputMismatchException;
+import java.util.concurrent.ThreadPoolExecutor;
+
+
+public class server implements Runnable {
+    public JTextField text=new JTextField(15);
+    public JButton btn;
+    public boolean suspend=false;
+    public static int flag=0;
+    public static int[] PORTS={9005,9006,9007};
+    public synchronized void toSuspend(){
+        suspend=true;
+    }
+    public synchronized void toResume(){
+        notify();
+        suspend=false;
+    }
+    public void createServerGUI() throws UnknownHostException{
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        JFrame frame=new JFrame("文件传输");
+        frame.setSize(400,300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel panel=new JPanel();
+        JLabel label=new JLabel("已接收");
+        panel.add(label);
+        panel.add(text);
+        JPanel panel2=new JPanel();
+        JLabel label2=new JLabel("IP地址");
+        panel2.add(label2);
+        JTextField ipText=new JTextField(15);
+        panel2.add(ipText);
+        InetAddress addr=InetAddress.getLocalHost();
+        ipText.setText(addr.getHostAddress());
+        frame.add(panel, BorderLayout.NORTH);
+        frame.add(panel2, BorderLayout.CENTER);
+        btn=new JButton("中断");
+        JPanel panel3=new JPanel();
+        panel3.add(btn);
+        frame.add(panel3,BorderLayout.SOUTH);
+        frame.pack();
+        frame.setVisible(true);
+    }
+    public static void main(String[] args) throws UnknownHostException {
+
+        try {
+            final ServerSocket server = new ServerSocket(9004);
+            server s=new server();
+            Thread th = new Thread(new Runnable() {
+                public void run() {
+                    while (true) {
+                        try {
+                            System.out.println("开始监听客户端的请求...");
+                            Socket socket = server.accept();
+                            System.out.println("有客户端连接...");
+                            s.AssignServer(socket);
+                        } catch (Exception e) {
+                            System.out.println(e);
+                        }
+                    }
+                }
+            });
+            th.start(); //启动线程运行
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void run() {
+
+    }
+
+    public void AssignServer(Socket socket) {
+        byte[] inputByte = null;
+        int length = 0;
+        DataInputStream dis = null;
+        FileOutputStream fos = null;
+        StringBuffer sb=new StringBuffer();
+        try {
+            try {
+                dis = new DataInputStream(socket.getInputStream());
+                byte[] input = new byte[1];
+                String name="";
+                String path="D:\\recordLog.txt";
+                File file=new File(path);
+                if(!file.exists()){
+                    file.createNewFile();
+                }
+                //fos = new FileOutputStream(new File("D:\\recordLog.txt"),true);
+                inputByte = new byte[1];
+                String str="";
+                while ((length = dis.read(inputByte, 0, inputByte.length)) > 0) {
+                  //  fos.write(inputByte, 0, length);
+                    str=str+new String(inputByte);
+                }
+                String s=str.replaceAll("[?]"," ");
+                System.out.println(s);
+                FileWriter fw=null;
+                fw=new FileWriter(file,true);
+                BufferedWriter out=new BufferedWriter(fw);
+                out.append(s+"\r\n",0,s.length()+2);
+                out.close();
+                String[] arr=str.split("[?]");
+//                System.out.println(arr[1]);
+                System.out.println("完成日志写入");
+                System.out.println("正在分配服务器");
+                Socket sk=new Socket();
+                sk.connect(new InetSocketAddress(arr[1], 9005));
+                if(flag>=2){
+                    flag=0;
+                }else{
+                    flag+=1;
+                }
+                System.out.println("当前分配服务器端口为"+PORTS[flag]);
+                OutputStream outStream=sk.getOutputStream();
+                outStream.write(arr[1].getBytes());
+                outStream.write("?".getBytes());
+                outStream.write(arr[2].getBytes());
+                sk.close();
+                outStream.close();
+            } finally {
+                if (dis != null)
+                    dis.close();
+                if (socket != null)
+                    socket.close();
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+}
+
